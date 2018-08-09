99f5847521250ce157a9f248ea64aa79122dfbd7
Matt Krepp <mattkrepp@gmail.com>
2018-08-09 14:41:05 -0400

    camera now works and displays the image that was captured (forever)

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CameraView from './Components/Camera';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      // <CameraView />
+      <Text>Here's some text. Commented out the cameraview component for commit</Text>
     );
   }
 }
