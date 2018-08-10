424d4eb5fa6926a773708afdd81d4cd7c6212db0
Jordan Langsam <jlangsam89@gmail.com>
2018-08-10 17:13:42 -0400

    added a tabBar

--- mobile/App.js
+++ mobile/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TabBarIOS } from 'react-native';
 import CameraView from './Components/Camera';
 import CameraButton from './Components/CameraButton';
 import WikipediaWebView from './Components/WikipediaWebView';
 
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      selectedTab: 'Home'
+    };
+  }
 
   render() {
     return (
-      // <CameraView />
-      <CameraButton />
+      <TabBarIOS selectedTab={this.state.selectedTab}>
+
+      <TabBarIOS.Item
+          selected={this.state.selectedTab === 'Home'}
+          onPress={() => {
+              this.setState({
+                  selectedTab: 'Home',
+              });
+          }} title='Home' icon={require('./assets/homeIcon.png')}>
+            <CameraView />
+            </TabBarIOS.Item>
+
+            <TabBarIOS.Item
+          selected={this.state.selectedTab === 'Results'}
+          onPress={() => {
+              this.setState({
+                  selectedTab: 'Results',
+              });
+          }} title='Results' icon={require('./assets/resultIcon.png')}>
+            <WikipediaWebView/>
+            </TabBarIOS.Item>
+
+        </TabBarIOS>
     );
   }
 }
