87cad354ca829ef3b4c5ba44739d36bfcc497a5a
johnnycheng14 <johnnycheng14@gmail.com>
2018-08-16 17:57:19 -0400

    testing

--- mobile/App.js
+++ mobile/App.js
@@ -19,56 +19,57 @@ export default class App extends React.Component {
 
 	render() {
 		return (
-			<TabBarIOS selectedTab={this.state.selectedTab}>
-			<TabBarIOS.Item
-					selected={this.state.selectedTab === "Login"}
-					onPress={() => {
-						this.setState({
-							selectedTab: "Login"
-						});
-					}}
-					title="Login"
-					icon={require("./assets/homeIcon.png")}
-				>
-					<LoginScreen />
-				</TabBarIOS.Item>
+			<PastLandMarks />
+			// <TabBarIOS selectedTab={this.state.selectedTab}>
+			// <TabBarIOS.Item
+			// 		selected={this.state.selectedTab === "Login"}
+			// 		onPress={() => {
+			// 			this.setState({
+			// 				selectedTab: "Login"
+			// 			});
+			// 		}}
+			// 		title="Login"
+			// 		icon={require("./assets/homeIcon.png")}
+			// 	>
+			// 		<LoginScreen />
+			// 	</TabBarIOS.Item>
 
-				<TabBarIOS.Item
-					selected={this.state.selectedTab === "Camera"}
-					onPress={() => {
-						this.setState({
-							selectedTab: "Camera"
-						});
-					}}
-					title="Camera"
-					icon={require("./assets/cameraIcon.png")}
-				>
-					<CameraView />
-				</TabBarIOS.Item>
+			// 	<TabBarIOS.Item
+			// 		selected={this.state.selectedTab === "Camera"}
+			// 		onPress={() => {
+			// 			this.setState({
+			// 				selectedTab: "Camera"
+			// 			});
+			// 		}}
+			// 		title="Camera"
+			// 		icon={require("./assets/cameraIcon.png")}
+			// 	>
+			// 		<CameraView />
+			// 	</TabBarIOS.Item>
 
-				<TabBarIOS.Item
-					selected={this.state.selectedTab === "Results"}
-					onPress={() => {
-						this.setState({
-							selectedTab: "Results"
-						});
-					}}
-					title="Results"
-					icon={require("./assets/searchIcon.png")}
-				>
-					<WikipediaWebView />
-				</TabBarIOS.Item>
-				<TabBarIOS.Item
-					selected={this.state.selectedTab === "Landmarks"}
-					onPress={() => {
-						this.setState({
-							selectedTab: "Landmarks"
-						});
-					}}
-					title="Landmarks">
-					 <LandmarksNearMe />
-				</TabBarIOS.Item>
-			</TabBarIOS>
+			// 	<TabBarIOS.Item
+			// 		selected={this.state.selectedTab === "Results"}
+			// 		onPress={() => {
+			// 			this.setState({
+			// 				selectedTab: "Results"
+			// 			});
+			// 		}}
+			// 		title="Results"
+			// 		icon={require("./assets/searchIcon.png")}
+			// 	>
+			// 		<WikipediaWebView />
+			// 	</TabBarIOS.Item>
+			// 	<TabBarIOS.Item
+			// 		selected={this.state.selectedTab === "Landmarks"}
+			// 		onPress={() => {
+			// 			this.setState({
+			// 				selectedTab: "Landmarks"
+			// 			});
+			// 		}}
+			// 		title="Landmarks">
+			// 		 <LandmarksNearMe />
+			// 	</TabBarIOS.Item>
+			// </TabBarIOS>
 		);
 	}
 }
