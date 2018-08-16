792692612140f3f59d84ff2c6f4ee9fdcf4834d9
johnnycheng14 <johnnycheng14@gmail.com>
2018-08-16 12:37:35 -0400

    completed yelp API route

--- web/server/api/yelp.js
+++ web/server/api/yelp.js
@@ -1,20 +1,31 @@
-const router = require('express')
+const router = require('express').Router()
 const axios = require('axios')
 
-router.get('/', async (req, res, next) => {
-  try {
-    await axios.post('/', 'molinari-delicatessen-san-francisco')
-  } catch (error) {
-    next(error)
-  }
-})
+// just a test route for the POST route
+// router.get('/', async (req, res, next) => {
+//   try {
+//     let location = {
+//       latitude: 40.704886099999996,
+//       longitude: -74.00914379999999}
+//     const restaurant = await axios.post('http://localhost:8080/api/yelp', {query:'McDonalds', location})
+//     res.send(restaurant)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 router.post('/', async (req, res, next) => {
   try {
-    const query = req.body
-    const response = await axios.get(`https://api.yelp.com/v3/businesses/search/${query}`)
-    const id = response.businesses[0].id
-    const restaurant = await axios.get(`https://api.yelp.com/v3/businesses/${id}`)
+    const {query, latitude, longitude} = req.body
+    console.log("query", query)
+    const instance = axios.create({
+      baseURL: 'https://api.yelp.com/v3/businesses/',
+      headers: {Authorization: `Bearer ${process.env.Yelp_API_Key}`}
+    })
+    const response = await instance.get(`/search?term=${query}&latitude=${latitude}&longitude=${longitude}&limit=10`)
+    console.log('response', response)
+    const restaurant = response.data.businesses[0]
+    console.log(restaurant)
     res.json(restaurant)
   } catch (error) {
     next(error)
