26311020d2c0a01a0ab56f2c06a01c08e0c7f1ea
johnnycheng14 <johnnycheng14@gmail.com>
2018-08-17 11:35:55 -0400

    minor changes fixes and redundancy removal

--- mobile/Components/SingleLandMark.js
+++ mobile/Components/SingleLandMark.js
@@ -44,9 +44,8 @@ export default class SingleLandMark extends React.Component {
         <Text>Stars</Text>
         <TextInput 
           style={{ height: 40, width: 40, borderColor: 'black', borderWidth: 1, textAlign: "center" }} 
-          onChangeText={rating => this.setState({rating})} 
-          value={this.state.rating} 
-          placeholder={""}/>
+          onChangeText={rating => this.setState({rating: Number(rating)})} 
+          value={'' + this.state.rating} />
         {/* <Picker
           selectedValue={this.state.rating}
           style={{width: 40}}
@@ -62,8 +61,7 @@ export default class SingleLandMark extends React.Component {
         <TextInput 
           style={{ height: 40, width: 300, borderColor: 'black', borderWidth: 1, textAlign: "center" }} 
           onChangeText={comment => this.setState({comment})} 
-          value={this.state.comment} 
-          placeholder={this.state.comment}/>
+          value={this.state.comment} />
         <Button onPress={this.update} title='Submit review'/>
       </View>
     )
