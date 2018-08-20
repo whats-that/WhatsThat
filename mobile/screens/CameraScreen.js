0e171204f6b57ac88428347739771334a217a800
J Song <feelpnw@gmail.com>
2018-08-20 17:35:10 -0400

    organized

--- mobile/screens/CameraScreen.js
+++ mobile/screens/CameraScreen.js
@@ -150,6 +150,7 @@ class CameraScreen extends React.Component {
       this.setState({
         loading: false,
       });
+      alert('process failure!')
     }, 5000);
     console.log(this.state.textDetection);
     // const imageFile = new File(this.state.previewSource)
