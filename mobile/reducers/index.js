c38a8fcf14c55326a950e23ebc579ddcfa390bf3
Jordan Langsam <jlangsam89@gmail.com>
2018-08-20 15:57:59 -0400

    added search string to redux

--- mobile/reducers/index.js
+++ mobile/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import landmark from './landmark';
 import thing from './thing';
+import searchString from './searchString';
 // import location from './location'
 
 export default combineReducers({
-  auth, landmark, thing
+  auth, landmark, thing, searchString
   // , location
 });
